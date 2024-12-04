62e15e458d041900e43ae6c57e670160adbe537f
MrBadAxe <mrbadaxe@gmail.com>
2024-12-04 00:41:55 -0500

    day 04: create WordSearch class

---
+++ WordSearch.java
@@ -0,0 +1,36 @@
+public class WordSearch extends CharGrid{
+    public WordSearch(int x, int y, char init){
+        super(x,y,init);
+    }
+
+    public int getXmasCount(int row, int col){
+        if(this.get(row, col) != 'X'){
+            return 0;
+        }
+        int z = 0;
+        if(row >= 3){
+            if(                                 this.get(row-1,col) == 'M' &&   this.get(row-2,col) == 'A' &&   this.get(row-3,col) == 'S'){ z++; }
+            if(col >= 3 &&                      this.get(row-1,col-1) == 'M' && this.get(row-2,col-2) == 'A' && this.get(row-3,col-3) == 'S'){ z++; }
+            if(col < this.getWidth() - 3 &&     this.get(row-1,col+1) == 'M' && this.get(row-2,col+2) == 'A' && this.get(row-3,col+3) == 'S'){ z++; }
+        }
+        if(row < this.getHeight() - 3){
+            if(                                 this.get(row+1,col) == 'M' &&   this.get(row+2,col) == 'A' &&   this.get(row+3,col) == 'S'){ z++; }
+            if(col >= 3 &&                      this.get(row+1,col-1) == 'M' && this.get(row+2,col-2) == 'A' && this.get(row+3,col-3) == 'S'){ z++; }
+            if(col < this.getWidth() - 3 &&     this.get(row+1,col+1) == 'M' && this.get(row+2,col+2) == 'A' && this.get(row+3,col+3) == 'S'){ z++; }
+        }
+        if(col >= 3 &&                          this.get(row,col-1) == 'M' &&   this.get(row,col-2) == 'A' &&   this.get(row,col-3) == 'S'){ z++; }
+        if(col < this.getWidth() - 3 &&         this.get(row,col+1) == 'M' &&   this.get(row,col+2) == 'A' &&   this.get(row,col+3) == 'S'){ z++; }
+
+        return z;
+    }
+
+    public int getTotalXmasCount(){
+        int total = 0;
+        for(int row=0; row<this.getHeight(); row++){
+            for(int col=0; col<this.getWidth(); col++){
+                total += getXmasCount(row, col);
+            }
+        }
+        return total;
+    }
+}
