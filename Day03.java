8dd3074b3bd7577db24ad364046a514d4879d610
MrBadAxe <mrbadaxe@gmail.com>
2024-12-03 23:31:08 -0500

    day 03: part 02

--- Day03.java
+++ Day03.java
@@ -16,4 +16,25 @@ public class Day03{
         }
         return Integer.toString(total);
     }
+
+    public static String getPart02(List<String> input){
+        int total = 0;
+        String str = "";
+        for(String s : input){
+            str += s;
+        }
+        str = "do()" + str + "don't()";
+        Pattern p1 = Pattern.compile("do\\(\\)(.*?)don't\\(\\)");
+        Matcher m1 = p1.matcher(str);
+        while(m1.find()){
+            Pattern p2 = Pattern.compile("mul\\((\\d+),(\\d+)\\)");
+            Matcher m2 = p2.matcher(m1.group());
+            while(m2.find()){
+                int op1 = Integer.parseInt(m2.group(1));
+                int op2 = Integer.parseInt(m2.group(2));
+                total += (op1 * op2);
+            }
+        }
+        return Integer.toString(total);
+    }    
 }
