bb35a1c4d50a9fb4548c5a98108abce8eda0cfcd
MrBadAxe <mrbadaxe@gmail.com>
2024-12-08 15:33:27 -0500

    day 08: findAllAntinodes method

--- Antenna.java
+++ Antenna.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Antenna extends Point{
     private final char frequency;
 
@@ -14,6 +17,19 @@ public class Antenna extends Point{
         long dx = other.getX() - this.getX();
         long dy = other.getY() - this.getY();
         return new Point(other.getX() + dx, other.getY() + dy);
+    }
+    public List<Point> findAllAntinodes(Antenna other, CharGrid map){
+        long dx = other.getX() - this.getX();
+        long dy = other.getY() - this.getY();
+        long n = 0;
+        ArrayList<Point> z = new ArrayList<>();
+        Point p = new Point(other.getX() + (dx * n), other.getY() + (dy * n));
+        while(p.getX() >= 0 && p.getX() < map.getHeight() && p.getY() >= 0 && p.getY() < map.getWidth()){
+            z.add(p);
+            n++;
+            p = new Point(other.getX() + (dx * n), other.getY() + (dy * n));
+        }
+        return z;
     }
 
     public String toString(){
