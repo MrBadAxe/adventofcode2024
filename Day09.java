909a548733b6ed829e4bb9b10c53849de700cff1
MrBadAxe <mrbadaxe@gmail.com>
2024-12-09 23:09:52 -0500

    day 09: remove comment

--- Day09.java
+++ Day09.java
@@ -3,7 +3,6 @@ import java.util.List;
 public class Day09{
     public static String getPart01(List<String> input){
         String diskMap = input.get(0);
-        //int[] disk = generateDiskFromMap(diskMap);
         AmphipodDisk disk = new AmphipodDisk(diskMap);
         disk.compressDisk();
         return Long.toString(disk.getChecksum());
