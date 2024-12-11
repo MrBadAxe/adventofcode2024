9ee5f84b68fd16696b57a3b46f6ba1773881def9
MrBadAxe <mrbadaxe@gmail.com>
2024-12-11 01:57:54 -0500

    day 11: refactor to use HashMap<Long,Long> instead of ArrayList<Long>

--- Day11.java
+++ Day11.java
@@ -1,20 +1,36 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Day11{
-    public static ArrayList<Long> blink(ArrayList<Long> stones){
-        ArrayList<Long> z = new ArrayList<>();
-        for(long stone : stones){
+    public static HashMap<Long,Long> blink(HashMap<Long,Long> stones){
+        HashMap<Long,Long> z = new HashMap<>();
+        for(long stone : stones.keySet()){
             if(stone == 0){
-                z.add(1L);
+                if(z.get(1L) == null){
+                    z.put(1L,0L);
+                }
+                z.put(1L,z.get(1L)+stones.get(stone));
             }else if(Long.toString(stone).length() % 2 == 0){
                 String str = Long.toString(stone);
-                String newStone1 = str.substring(0,str.length()/2);
-                String newStone2 = str.substring(str.length()/2,str.length());
-                z.add(Long.parseLong(newStone1));
-                z.add(Long.parseLong(newStone2));
+                long newStone1 = Long.parseLong(str.substring(0,str.length()/2));
+                long newStone2 = Long.parseLong(str.substring(str.length()/2,str.length()));
+
+                if(z.get(newStone1) == null){
+                    z.put(newStone1,0L);
+                }
+                z.put(newStone1,z.get(newStone1)+stones.get(stone));
+
+                if(z.get(newStone2) == null){
+                    z.put(newStone2,0L);
+                }
+                z.put(newStone2,z.get(newStone2)+stones.get(stone));
             }else{
-                z.add(stone * 2024);
+                long newStone = stone * 2024;
+                if(z.get(newStone) == null){
+                    z.put(newStone,0L);
+                }
+                z.put(newStone,z.get(newStone)+stones.get(stone));
             }
         }
         return z;
@@ -22,16 +38,24 @@ public class Day11{
     
     public static String getPart01(List<String> input){
         String startingStones = input.get(0);
-        ArrayList<Long> stones = new ArrayList<>();
+        HashMap<Long,Long> stones = new HashMap<>();
         String[] split = startingStones.split("\s+");
         for(String str : split){
-            stones.add(Long.parseLong(str));
+            long newStone = Long.parseLong(str);
+            if(stones.get(newStone) == null){
+                stones.put(newStone,0L);
+            }
+            stones.put(newStone,stones.get(newStone)+1L);
         }
         System.out.println(stones);
         for(int k=0;k<25;k++){
             stones = blink(stones);
             System.out.println(stones);
         }
-        return Integer.toString(stones.size());
+        long total = 0;
+        for(long stone : stones.keySet()){
+            total += stones.get(stone);
+        }
+        return Long.toString(total);
     }
 }
