d6add63ed8c3f1730e9b6e0aeee10e6d31b499ad
MrBadAxe <mrbadaxe@gmail.com>
2024-12-12 23:52:48 -0500

    day 12: remove whitespace and comments

--- Day12.java
+++ Day12.java
@@ -4,15 +4,11 @@ import java.util.ArrayList;
 public class Day12{
     public static String getPart01(List<String> input){
         GardenMap map = new GardenMap(input);
-        
         ArrayList<GardenMapRegion> regions = map.generateRegionsList();
-
         int total = 0;
         for(GardenMapRegion region : regions){
-            //System.out.println(region);
             total += region.calculateFencingCost();
         }
-
         return Integer.toString(total);
     }
     public static String getPart02(List<String> input){
