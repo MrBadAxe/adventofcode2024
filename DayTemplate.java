98b85a601d9c64123bbb73754e0450fbd861d2e2
MrBadAxe <mrbadaxe@gmail.com>
2024-12-17 01:26:38 -0500

    template the Day classes

---
+++ DayTemplate.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class DayTemplate{
+    public static String getPart01(List<String> input){
+        return "";
+    }
+    public static String getPart02(List<String> input){
+        return "";
+    }
+
+}
