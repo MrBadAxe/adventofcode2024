b4938a7a710845fd8b612ba5fe3851595169bb10
MrBadAxe <mrbadaxe@gmail.com>
2024-12-18 15:04:39 -0500

    day 18: part 02

--- AdventOfCode2024.java
+++ AdventOfCode2024.java
@@ -69,9 +69,9 @@ public class AdventOfCode2024{
     */
 
     System.out.println("Day 18 Part 01: " + Day18.getPart01(readInput("./input_day18.txt")));
-    /*
     System.out.println("Day 18 Part 02: " + Day18.getPart02(readInput("./input_day18.txt")));
-    
+
+    /*
     System.out.println("Day 19 Part 01: " + Day19.getPart01(readInput("./input_day19.txt")));
     System.out.println("Day 19 Part 02: " + Day19.getPart02(readInput("./input_day19.txt")));
     
