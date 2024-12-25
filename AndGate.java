cf970748a78c13a10110d47347c890017bda00de
MrBadAxe <mrbadaxe@gmail.com>
2024-12-25 18:00:48 -0500

    day 24: implementation

--- AndGate.java
+++ AndGate.java
@@ -19,10 +19,30 @@ public class AndGate implements LogicSource{
     public String getLabel(){
         return this.label;
     }
+    public void setLabel(String newLabel){
+        this.label = newLabel;
+    }
     public boolean getOutput(){
         return (input1.getOutput() && input2.getOutput());
     }
     public String toString(){
-        return this.getLabel() + ":(" + input1.toString() + " AND " + input2.toString() + ")";
+        return this.getLabel() + "(" + this.getBranchWeight() + "):(" + input1.toString() + " AND " + input2.toString() + ")";
+    }
+    public int getBranchWeight(){
+        return input1.getBranchWeight() + input2.getBranchWeight();
+    }
+    public boolean isCarryFlag(){
+        return (input1 instanceof StaticSource && input1 instanceof StaticSource);
+    }
+    public boolean needsSwap(){
+        if(this.label.charAt(0) == 'z'){ return true; }
+        if(isCarryFlag()){ return false; }
+        
+        if(input1 instanceof XorGate && ((XorGate)input1).isHalfAdder() && input2 instanceof OrGate){ return false;}
+        if(input2 instanceof XorGate && ((XorGate)input2).isHalfAdder() && input1 instanceof OrGate){ return false;}
+
+        if(input1 instanceof XorGate && input2 instanceof AndGate && ((AndGate)input2).isCarryFlag()){ return false; }
+        if(input2 instanceof XorGate && input1 instanceof AndGate && ((AndGate)input1).isCarryFlag()){ return false; }
+        return true;
     }
 }
